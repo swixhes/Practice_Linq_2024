b24eafc5d74978fd83c6fe14926d39209aefcf75
Alona Chuchupalova <a.s.chuchupalova@student.khai.edu>
2024-12-20 11:53:01 +0200

    Query 3

--- Practice_Linq_2024/Program.cs
+++ Practice_Linq_2024/Program.cs
@@ -86,12 +86,18 @@ namespace Practice_Linq_2024
         {
             //Query 3: Вивести всі домашні матчі збірної Франції за 2021 рік, де вона зіграла у нічию.
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games.Where(g => g.Home_team == "France" &&
+                    g.Date.Year == 2021 &&
+                    g.Home_score == g.Away_score &&
+                    g.Country == "France");
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 3 ========================");
 
-            // див. приклад як має бути виведено:
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
 
 
         }
