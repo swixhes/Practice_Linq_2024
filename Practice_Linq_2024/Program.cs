2b32bae3f59df03e3cc25eb6d930119b9cd7b725
Alona Chuchupalova <a.s.chuchupalova@student.khai.edu>
2024-12-20 12:12:54 +0200

    Query 6

--- Practice_Linq_2024/Program.cs
+++ Practice_Linq_2024/Program.cs
@@ -154,13 +154,17 @@ namespace Practice_Linq_2024
             //Query 6: Вивести всі матчі останнього чемпіоната світу з футболу (FIFA World Cup), починаючи з чвертьфіналів (тобто останні 8 матчів).
             //Матчі мають відображатися від фіналу до чвертьфіналів (тобто у зворотній послідовності).
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games.Where(g => g.Tournament == "FIFA World Cup" && g.Date.Year == 2022)
+                .OrderByDescending(g => g.Date).Take(8);
 
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 6 ========================");
 
-            // див. приклад як має бути виведено:
+            foreach (var game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date:dd.MM.yyyy} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
 
 
         }
